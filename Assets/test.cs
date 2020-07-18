07b79003b1321e122e0265d7f087bc1f9354e501
Leonardo Chieppe <chieppe2142@gmail.com>
2020-07-18 00:03:39 -0300

    first

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    Rigidbody rg;
+    public Transform parent;
+    public float torque;
+    // Start is called before the first frame update
+    void Start() {
+        rg = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,parent.eulerAngles.y,0);
+        rg.AddTorque(this.transform.right * torque);
+        //rg.AddForce(parent.forward*torque, ForceMode.Acceleration);
+        //transform.Rotate(new Vector3(10,0,0),Space.Self);
+        //transform.Rotate(new Vector3(0,parent.rotation.y,0),Space.World);
+        
+    }
+}
---
+++ Assets/test2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour {
+    public Transform child;
+    public float turn;
+    // Start is called before the first frame update
+    void Start() {
+        //child=transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.position = child.position;
+        this.transform.Rotate(new Vector3(0,1,0)*turn,Space.World);
+    }
+}
